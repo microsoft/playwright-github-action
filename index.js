2595a41e76565f5fce5ce5a0cf37cd74a3c60ec2
Arjun Attam <arjunattam@gmail.com>
2020-04-08 14:38:14 -0700

    add awaits

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const os = require('os');
 async function run() {
   try {
     if (os.platform() === 'linux') {
-      exec('sudo', ['apt-get', 'update']);
+      await exec('sudo', ['apt-get', 'update']);
       // For Chromium
-      exec('sudo', ['apt-get', 'install', 'libgbm-dev']);
+      await exec('sudo', ['apt-get', 'install', 'libgbm-dev']);
       // For WebKit
-      exec('sudo', ['apt-get', 'install', 'libwoff1',
-                                          'libwoff-dev',
-                                          'libopus0',
-                                          'libwebp6',
-                                          'libwebpdemux2',
-                                          'libenchant1c2a',
-                                          'libgudev-1.0-0',
-                                          'libsecret-1-0',
-                                          'libhyphen0',
-                                          'libgdk-pixbuf2.0-0',
-                                          'libegl1',
-                                          'libgles2',
-                                          'libevent-2.1-6',
-                                          'libnotify4',
-                                          'libxslt1.1']);
+      await exec('sudo', ['apt-get', 'install', 'libwoff1',
+                                                'libwoff-dev',
+                                                'libopus0',
+                                                'libwebp6',
+                                                'libwebpdemux2',
+                                                'libenchant1c2a',
+                                                'libgudev-1.0-0',
+                                                'libsecret-1-0',
+                                                'libhyphen0',
+                                                'libgdk-pixbuf2.0-0',
+                                                'libegl1',
+                                                'libgles2',
+                                                'libevent-2.1-6',
+                                                'libnotify4',
+                                                'libxslt1.1']);
     }
   } 
   catch (error) {
